832bd3578d44dd5d37f400dcbfa2bf3e2f9da464
dkul108 <dkul108@gmail.com>
2019-12-19 11:28:14 +0100

    routers

--- src/App.js
+++ src/App.js
@@ -3,27 +3,38 @@ import logo from './logo.svg'
 import {render} from 'react-dom';
 import PropTypes from 'prop-types';
 
+
 require("bootstrap/dist/css/bootstrap.css");
 
 const dataSource = [
-    {firstName: "John", lastName: "Doe", active: false},
-    {firstName: "Mary", lastName: "Moe", active: false},
-    {firstName: "Peter", lastName: "Noname", active: true}
+    {firstName: "John", lastName: "Doe", active: false, id: 1},
+    {firstName: "Mary", lastName: "Moe", active: false, id: 2},
+    {firstName: "Peter", lastName: "Noname", active: true, id: 3}
 ]
 
 class GridRecord extends React.Component {
     render() {
         let {record} = this.props;
         return <tr>
-            <th>
-                <span onChange={this.props.handleFilterChange} onDoubleClick={this.props.handleFilterChange}>{record.firstName} </span>
+            <th onClick={this.showUserDetails.bind(this)}>
+                <a href="#">{record.id}</a>
             </th>
             <th>
-                <span onChange={this.props.handleFilterChange} onDoubleClick={this.props.handleFilterChange}>{record.lastName} </span>
+                <span onChange={this.props.handleFilterChange}
+                      onDoubleClick={this.props.handleFilterChange}>{record.firstName} </span>
+            </th>
+            <th>
+                <span onChange={this.props.handleFilterChange}
+                      onDoubleClick={this.props.handleFilterChange}>{record.lastName} </span>
             </th>
             <th><input type="checkbox" checked={record.active}
                        onChange={this.props.toggleActive}/></th>
         </tr>
+    }
+
+    showUserDetails(e) {
+        e.preventDefault();
+        this.props.history.push(`/details/${this.props.record.id}`);
     }
 }
 
@@ -40,20 +51,102 @@ GridRecord.propTypes = {
 };
 
 export class SummaryActive extends React.Component {
-    render(){
+    render() {
         return (
             <div>Active Users:
-                {this.props.records.filter((record)=>record.active).length}</div>
+                {this.props.records.filter((record) => record.active).length}</div>
         )
     }
 }
+
 export class SummaryUsers extends React.Component {
-    render(){
+    render() {
         return (
             <div>Users Count: {this.props.records.length}</div>
         )
     }
 }
+
+
+function User({detail}) {
+    return (<div className="well profile">
+        <div className="col-sm-12">
+            <div className="col-xs-12 col-sm-8">
+                <h2>{detail.name}</h2>
+                <p><strong>About:</strong>{detail.about}</p>
+                <p><strong>Hobby:</strong>{detail.hobby}</p>
+                <p><strong>Skills: </strong>
+                    {detail.skills.map(skill => <span>{skill}, </span>)}
+                </p>
+            </div>
+            <div className="col-xs-12 col-sm-4 text-center">
+                <figure>
+                    <img
+                        src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-2.jpg"
+                        alt="" className="img-circle img-responsive"/>
+                </figure>
+            </div>
+        </div>
+    </div>)
+}
+
+export class UserDetails extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            details: []
+        }
+    }
+
+    filterDetails(id) {
+        if (id) {
+            this.setState({
+                details: detailsRecords.filter((record) => {
+                    return record.id == id;
+                })
+            })
+        } else {
+            this.setState({
+                details: detailsRecords
+            })
+        }
+    }
+
+    componentWillMount() {
+        this.filterDetails(this.props.match.params.id);
+    }
+
+    componentWillReceiveProps(props) {
+        this.filterDetails(props.match.params.id);
+    }
+
+    render() {
+        let {details} = this.state;
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+                        {details.map(detail => <User detail={detail}/>)}
+                    </div>
+                </div>
+            </div>)
+    }
+}
+
+const detailsRecords = [{
+    id: 1,
+    name: "John Doe",
+    about: "Nice guy",
+    hobby: "Likes drinking wine",
+    skills: ["html", "javascript", "redux"]
+}, {
+    id: 2,
+    name: "Mary Moe",
+    about: "Cute girl",
+    hobby: "Likes playing xbox whole days long",
+    skills: ["Fortran", "Lua", "R#"]
+}];
 
 export default class GridComponent extends React.Component {
     constructor(props) {
@@ -63,10 +156,10 @@ export default class GridComponent extends React.Component {
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.refs.filterInput && this.refs.filterInput.focus();
         this.setState({
-            records:dataSource
+            records: dataSource
         })
     }
 
@@ -92,11 +185,12 @@ export default class GridComponent extends React.Component {
             <div style={{width: 300, height: 300, padding: 20}}>
                 <p>
                     <input type="text" ref="filterInput" placeholder="Filter by..."
-                           onChange={(e)=> this.handleFilterChange(e)}/>
+                           onChange={(e) => this.handleFilterChange(e)}/>
                 </p>
                 <table className="table table-condensed">
                     <thead>
                     <tr>
+                        <th>Id</th>
                         <th>First Name</th>
                         <th>Last Name</th>
                         <th>Active</th>
@@ -104,14 +198,27 @@ export default class GridComponent extends React.Component {
                     </thead>
                     <tbody>
                     {this.state.records.map((record, index) => {
-                        return <GridRecord record={record} key={index} toggleActive={this.toggleActive.bind(this, index)}/>
+                        return <GridRecord record={record} key={index} history={this.props.history}
+                                           toggleActive={this.toggleActive.bind(this, index)}/>
                     })}
                     </tbody>
                 </table>
                 <div>{this.props.children &&
-                React.cloneElement(this.props.children, { records: this.state.records })}</div>
+                React.cloneElement(this.props.children, {records: this.state.records})}</div>
             </div>
         )
     }
 }
+
+
+// <HashRouter>
+//     <div>
+//         <Header />
+//         <Switch>
+//             <Route path="/grid" component={GridComponent}/>
+//             <Route exact path="/details" component={UserDetails}/>
+//             <Route path="/details/:id" component={UserDetails}/>
+//         </Switch>
+//     </div>
+// </HashRouter>
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import GridComponent from './App';
-import {SummaryActive, SummaryUsers} from './App';
+import {SummaryActive, SummaryUsers, UserDetails} from './App';
 import * as serviceWorker from './serviceWorker';
+import {HashRouter, Switch, Route, Link} from "react-router-dom";
 
 function Summary(props){
     return (<div>
@@ -12,7 +13,27 @@ function Summary(props){
     </div>)
 }
 
-ReactDOM.render(<GridComponent><Summary/></GridComponent>, document.getElementById('root'));
+
+const Header = ({children}) =>
+    <div>
+        <h1>Our awesome app</h1>
+        <ul role="nav">
+            <li><Link to="/grid">Grid</Link></li>
+            <li><Link to="/details">Details</Link></li>
+        </ul>
+        {children}
+    </div>;
+
+ReactDOM.render(<HashRouter>
+    <div>
+        <Header />
+        <Switch>
+            <Route path="/grid" component={GridComponent}/>
+            <Route exact path="/details" component={UserDetails}/>
+            <Route path="/details/:id" component={UserDetails}/>
+        </Switch>
+    </div>
+</HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
